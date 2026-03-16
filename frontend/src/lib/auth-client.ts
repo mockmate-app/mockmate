713deed53e702498c3f112b3c531998179303478
Indrajit <indrajitvijayakumar@gmail.com>
2026-03-16 19:06:21 +0530

    - Removed Polar interfering with new account creation

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,10 +1,8 @@
-import { createAuthClient } from "better-auth/react"
-import { polarClient } from "@polar-sh/better-auth/client";  
+import { createAuthClient } from "better-auth/react" 
 
 const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.BETTER_AUTH_URL,
-    plugins: [polarClient()], 
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
